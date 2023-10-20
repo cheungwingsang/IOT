019233a7d06f884826ad9b020e98a97ef3239711
Leo Cheung <21217351@life.hkbu.edu.hk>
2023-10-20 15:12:55 +0800

    Lab 4 initial commit

---
+++ IOT/Preview Content/BleView.swift
@@ -0,0 +1,18 @@
+//
+//  BleView.swift
+//  IOT
+//
+//  Created by Leo Cheung on 20/10/2023.
+//
+
+import SwiftUI
+
+struct BleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BleView()
+}
