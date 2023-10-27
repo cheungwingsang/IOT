ae3d8042c168582ee26d2dbbfd10568b842fa8c7
Leo Cheung <21217351@life.hkbu.edu.hk>
2023-10-27 14:57:23 +0800

    Lab 5 Swift Chart

---
+++ IOT/Preview Content/StaticChart.swift
@@ -0,0 +1,18 @@
+//
+//  StaticChart.swift
+//  IOT
+//
+//  Created by Leo Cheung on 27/10/2023.
+//
+
+import SwiftUI
+
+struct StaticChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StaticChart()
+}
---
+++ IOT/Preview Content/TallyView.swift
@@ -0,0 +1,18 @@
+//
+//  TallyView.swift
+//  IOT
+//
+//  Created by Leo Cheung on 27/10/2023.
+//
+
+import SwiftUI
+
+struct TallyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TallyView()
+}
